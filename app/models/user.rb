66141e54722421e0face06a116190d006eeae0de
Scott Gardner <scott@inyago.com>
2013-04-05 17:47:51 -0500

    Complete 6.5 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-	before_save {self.email = email.downcase}
-	validates(:name, {presence: true, length: {maximum: 50}})
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates(:email, {presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}})
 	has_secure_password
+	before_save {self.email = email.downcase!}
+	validates(:name, {presence: true, length: {maximum: 50}})
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # Allows foo@bar..com
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates(:email, {presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}})
 	validates(:password, {length: {minimum: 6}})
 	validates(:password_confirmation, {presence: true})
 end
